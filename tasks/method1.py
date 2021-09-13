95a929aacec1c0f8907d9c5f9717ecada06d21c1
gotoindex <s.t.100.rus.version@gmail.com>
2021-09-14 01:17:55 +0300

    Feat(tasks): add task 1 solution

---
+++ tasks/method1.py
@@ -0,0 +1,36 @@
+import argparse
+
+
+class Chessboard:
+    """Main class of the program. Its purpose is to print out
+    a table of symbols imitating a chessboard.\n
+    Any negative inputs will be converted into positive ones.
+    ### Params:
+    - width - an integer amount of columns of the chessboard;
+    - height - an integer amount of rows of the chessboard.
+    """
+
+    def __init__(self, width, height):
+        self.width = abs(width)
+        self.height = abs(height)
+        self.odd_line = ''.join(tuple('* ' for i in range(int(self.width / 2))))
+        if self.width % 2 != 0:
+            self.odd_line = self.odd_line + '*'
+        self.even_line = ' ' + self.odd_line[:-1]
+        self.show()
+
+    def show(self):
+        for i in range(int(self.height / 2)):
+            print(self.odd_line)
+            print(self.even_line)
+        if self.height % 2 != 0:
+            print(self.odd_line)
+
+
+parser = argparse.ArgumentParser(description='Display a custom-sized chessboard.')
+parser.add_argument('height', type=int,
+                    help='an integer amount of rows of the chessboard')
+parser.add_argument('width', type=int,
+                    help='an integer amount of columns of the chessboard')
+args = parser.parse_args()
+Chessboard(args.width, args.height)
